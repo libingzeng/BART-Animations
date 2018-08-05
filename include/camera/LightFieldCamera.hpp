b9c226c90e3ecd14310c3c31db06fb38aa3efd94
libingzeng <libingzeng123@gmail.com>
2018-08-05 21:04:07 +0800

    add some pictures

---
+++ include/camera/LightFieldCamera.hpp
@@ -0,0 +1,14 @@
+//
+//  LightFieldCamera.hpp
+//  BART-Animations
+//
+//  Created by libingzeng on 2018/8/4.
+//  Copyright © 2018 曾立兵. All rights reserved.
+//
+
+#ifndef LightFieldCamera_hpp
+#define LightFieldCamera_hpp
+
+#include <stdio.h>
+
+#endif /* LightFieldCamera_hpp */
---
+++ src/camera/LightFieldCamera.cpp
@@ -0,0 +1,9 @@
+//
+//  LightFieldCamera.cpp
+//  BART-Animations
+//
+//  Created by libingzeng on 2018/8/4.
+//  Copyright © 2018 曾立兵. All rights reserved.
+//
+
+#include "LightFieldCamera.hpp"
